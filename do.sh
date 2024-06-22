afa64b83b65d7e9c6e3a9834611ad792e060366a
azimut <azimut.github@protonmail.com>
2024-06-21 21:46:49 -0300

    update script

--- do.sh
+++ do.sh
@@ -1,15 +1,13 @@
-#!/bin/sh -e
+#!/bin/sh -exu
 
 mkdir -p public
 rm -vf ./public/*
-cd public/
 
-find . -type f -name '*.org' \
-	-exec mawk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
+find . -name '*.org' -exec awk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
 	while read -r file; do
 		emacs --batch --eval "(require 'org)" "${file}" --funcall org-html-export-to-html
 		case "$file" in
-		*self.org) mv -v "${file%.*}.html" "$(basename "$(dirname $file)").html" ;;
-		*) mv -v "${file%.*}.html" . ;;
+		*self.org) mv -v "${file%.*}.html" "public/$(basename "$(dirname $file)").html" ;;
+		*) mv -v "${file%.*}.html" public/ ;;
 		esac
 	done
