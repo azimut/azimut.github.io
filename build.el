aeefc162b6a2dd5b210bd89efe42dbd3b74db7f4
azimut <azimut.github@protonmail.com>
2024-06-22 16:14:39 -0300

    add date with format

--- build.el
+++ build.el
@@ -8,18 +8,22 @@
   (package-refresh-contents))
 (package-install 'htmlize)
 
+;;--------------------------------------------------
 
 (require 'ox-publish)
+
+(setf org-html-metadata-timestamp-format "%Y-%m-%d")
 (setq org-html-validation-link nil) ;; rm Validate link
 (setq org-html-head-include-scripts nil)
 (setq org-html-head-include-default-style nil)
-;; (setq org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>")
-;; (setq org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>")
-(setq org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>")
+(setq org-html-head
+      (concat "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>"
+              "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>"))
 (setq org-publish-project-alist
       (list
        (list
         "Notes" ; unique
+        :with-date t
         :auto-sitemap t
         :sitemap-filename "index.org"
         :sitemap-title "Notes"
@@ -33,5 +37,7 @@
         :with-toc t
         :section-numbers t
         :time-stamp-file nil)))
+
 (org-publish-all t) ; t for NO cache
+
 (message "Build complete")
