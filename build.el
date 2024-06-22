b63eb490d093409a948fb58fcd2efe59ed00e934
azimut <azimut.github@protonmail.com>
2024-06-22 04:20:48 -0300

    change build to use more .el, add styling

---
+++ build.el
@@ -0,0 +1,35 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("elpa"  . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-install 'htmlize)
+
+
+(require 'ox-publish)
+(setq org-html-validation-link nil) ;; rm Validate link
+(setq org-html-head-include-scripts nil)
+(setq org-html-head-include-default-style nil)
+(setq org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>")
+(setq org-publish-project-alist
+      (list
+       (list
+        "Notes" ; unique
+        :auto-sitemap t
+        :sitemap-filename "index.org"
+        :sitemap-title "Notes"
+        :sitemap-ignore-case t
+        :recursive t
+        :base-directory "./content" ;; .org
+        :publishing-directory "./public" ;; created
+        :publishing-function 'org-html-publish-to-html
+        :with-author nil
+        :with-creator t ; emacs and org versions
+        :with-toc t
+        :section-numbers t
+        :time-stamp-file nil)))
+(org-publish-all t) ; t for NO cache
+(message "Build complete")
--- do.sh
+++ do.sh
@@ -1,20 +1,20 @@
-#!/bin/bash -xeu
+#!/bin/bash
 
-mkdir -p public
-rm -vf ./public/*
+set -exuo pipefail
+
+rm -vrf ./public ./content
+mkdir -p public content
 
 realname() {
 	[[ $1 == *self.org ]] &&
-		echo "$(basename "$(dirname $1)").html" ||
-		echo "$(basename "$1" .org).html"
+		echo "$(basename "$(dirname $1)").org" ||
+		basename $1
 }
 
-echo "<html><body><ul>" >>public/index.html
-find -L . -name '*.org' \
-	-exec awk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
+find -L . -name '*.org' -exec awk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
+	grep -v content/ |
 	while read -r file; do
-		emacs --batch --eval "(require 'org)" "${file}" --funcall org-html-export-to-html
-		echo "<li><a href='$(realname $file)'>$(head -n1 "${file}" | cut -f2- -d' ')</a></li>" >>public/index.html
-		mv -v "${file%.*}.html" "public/$(realname "${file}")"
+		cp "${file}" "content/$(realname ${file})"
 	done
-echo "</ul></body></html>" >>public/index.html
+
+emacs -Q --script build.el
