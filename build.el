b22dddbe3348ae7044772cb56cac7b6950149f0e
azimut <azimut.github@protonmail.com>
2024-06-23 04:41:29 -0300

    working on the sitemap + date + preamble

--- build.el
+++ build.el
@@ -12,6 +12,10 @@
 
 (require 'ox-publish)
 
+(setq org-html-preamble-format
+      `(("en" ,(concat "<h1 class=\"title\">%t</h1>"
+                       "<p class=\"subtitle\">Date: %d</p>"
+                       "<p class=\"subtitle\">Last Modified: %C</p>"))))
 (setf org-html-metadata-timestamp-format "%Y-%m-%d")
 (setq org-html-validation-link nil) ;; rm Validate link
 (setq org-html-head-include-scripts nil)
@@ -19,24 +23,34 @@
 (setq org-html-head
       (concat "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>"
               "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>"))
+
 (setq org-publish-project-alist
-      (list
-       (list
-        "Notes" ; unique
-        :with-date t
-        :auto-sitemap t
-        :sitemap-filename "index.org"
-        :sitemap-title "Notes"
-        :sitemap-ignore-case t
-        :recursive t
-        :base-directory "./content" ;; .org
-        :publishing-directory "./public" ;; created
-        :publishing-function 'org-html-publish-to-html
-        :with-author nil
-        :with-creator t ; emacs and org versions
-        :with-toc t
-        :section-numbers t
-        :time-stamp-file nil)))
+      `(("Notes" ; unique
+         :auto-sitemap t
+         :sitemap-format-entry
+         ,(lambda (entry style project)
+            (format "%s [[file:%s][%s]]"
+                    (format-time-string "%Y-%m-%d" (org-publish-find-date entry project))
+                    entry
+                    (org-publish-find-title entry project)))
+         ;; :sitemap-function
+         ;; ,(lambda (title list)
+         ;;    (concat ;"#+OPTIONS: html-postamble:t html-preamble:nil"
+         ;;     (print (org-list-to-org (print list)))))
+         :sitemap-filename "index.org"
+         :sitemap-title "Notes"
+         :sitemap-sort-files anti-chronologically
+         :with-date nil
+         :recursive t
+         :base-directory "./content" ;; .org
+         :publishing-directory "./public" ;; created
+         :publishing-function org-html-publish-to-html
+         :with-author nil
+         :with-creator t ; emacs and org versions
+         :with-title nil
+         :with-toc t
+         :section-numbers t
+         :time-stamp-file nil)))
 
 (org-publish-all t) ; t for NO cache
 
