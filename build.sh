7ff2ef7b7f1570ef4e13861a1b5cab5c022e145d
azimut <azimut.github@protonmail.com>
2024-06-24 23:51:02 -0300

    restore git timestamp

--- build.sh
+++ build.sh
@@ -8,14 +8,23 @@ realname() {
 		basename $1
 }
 
+restore_timestamp() {
+	local file
+	file="$(echo "$1" | cut -f3- -d/)"
+	cd "$(echo "$1" | cut -f2 -d/)"
+	touch -d "$(git log -1 --format='%aI' ${file})" "${file}"
+	cd -
+}
+
 rm -vrf ./public ./content
 mkdir -p public content
 
 find -L . -name '*.org' -exec awk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
 	grep -v content/ |
 	while read -r file; do
+		restore_timestamp "${file}"
 		cp -p "${file}" "content/$(realname ${file})"
 	done
 
-cp -p style.css public/
+cp style.css public/
 emacs -Q --script build.el
