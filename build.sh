e3276fb4cd2c330d766e8244679d2e910ac1f87c
azimut <azimut.github@protonmail.com>
2025-01-07 18:02:08 -0300

    whitelist notes

--- build.sh
+++ build.sh
@@ -23,13 +23,17 @@ rm -vrf ./public/* ./org/notes
 mkdir -p public org/notes
 
 # org/notes/ - Copy .org files and media
-find -L . -name '*.org' -exec awk 'NR == 1 && /TITLE/ { print FILENAME }' {} \; |
-    grep -v org/ |
-    while read -r file; do
-        restore_timestamp "${file}"
-        cp -vp "${file}" "org/notes/$(realname "${file}")"
-        cp -vp "$(dirname "${file}")/"*.@(jpg|png|jpeg|gif) org/notes/ || true
-    done
+notes=(
+    ./programming-notes/terminal/languages/awk/awk.org
+    ./programming-notes/terminal/languages/sed/self.org
+    ./programming-notes/terminal/languages/jq/jq.org
+    ./programming-notes/languages/sql/sqlite/self.org
+)
+for note in "${notes[@]}"; do
+    restore_timestamp "${note}"
+    cp -vp "${note}" "org/notes/$(realname "${note}")"
+    cp -vp "$(dirname "${note}")/"*.@(jpg|png|jpeg|gif) org/notes/ || true
+done
 
 emacs -Q --script build.el
 
