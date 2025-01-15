235fe4c42c37e1bb13a529a2db1dd376c49ec7c5
azimut <azimut.github@protonmail.com>
2025-01-15 08:25:06 -0300

    ui: shuffle note tags on mainpage with js

--- org/script.js
+++ org/script.js
@@ -1,6 +1,19 @@
 window.onload = function () {
-  window.document.querySelectorAll("span.timestamp").forEach((span) => {
+  fixTimestamps();
+  sortNotes();
+};
+
+// Reference: https://stackoverflow.com/questions/25175798/how-to-shuffle-a-nodelist
+function sortNotes() {
+  const notes = document.querySelector("#text-notes ul");
+  for (let i = notes.children.length; i > 0; i--) {
+    notes.appendChild(notes.children[(Math.random() * i) | 0]);
+  }
+}
+
+function fixTimestamps() {
+  document.querySelectorAll("span.timestamp").forEach((span) => {
     const date = new Date(span.innerText.substr(1, 10));
     span.innerText = `${date.getFullYear() % 1000}/${date.getMonth()}`;
   });
-};
+}
