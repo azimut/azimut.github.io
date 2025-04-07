95ec308d884d33508901c9f79889b2367f691c8a
azimut <azimut.github@protonmail.com>
2025-04-07 16:00:17 -0300

    fix: js return when no notes

--- org/script.js
+++ org/script.js
@@ -5,6 +5,7 @@ window.onload = function () {
 // Reference: https://stackoverflow.com/questions/25175798/how-to-shuffle-a-nodelist
 function sortNotes() {
   const notes = document.querySelector(".notes ul");
+  if (!notes) return;
   for (let i = notes.children.length; i > 0; i--) {
     notes.appendChild(notes.children[(Math.random() * i) | 0]);
   }
