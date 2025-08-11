f26b8af1879154d36b1cc1ff2983a0e5ae43ea75
azimut <azimut.github@protonmail.com>
2025-08-11 13:34:21 -0300

    note: pic new split

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ notes=(
     ./programming-notes/terminal/languages/m4/self.org
     ./programming-notes/terminal/languages/dc.org
     ./programming-notes/terminal/languages/bc.org
-    ./programming-notes/terminal/tools/roff/pic.org
+    ./programming-notes/terminal/tools/roff/pic/self.org
     ./programming-notes/languages/stack/postscript/self.org
     # ./programming-notes/languages/sql/sqlite/self.org
     ./programming-notes/languages/c/makefile/self.org
