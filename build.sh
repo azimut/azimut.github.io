6e4eb6740bcca29173e4dd40a44402b6cbf27481
azimut <azimut.github@protonmail.com>
2025-08-22 18:26:40 -0300

    move dc/bc path

--- build.sh
+++ build.sh
@@ -29,8 +29,8 @@ notes=(
     ./programming-notes/terminal/languages/sed/self.org
     ./programming-notes/terminal/languages/jq/jq.org
     ./programming-notes/terminal/languages/m4/self.org
-    ./programming-notes/terminal/languages/dc.org
-    ./programming-notes/terminal/languages/bc.org
+    ./programming-notes/terminal/languages/dc/self.org
+    ./programming-notes/terminal/languages/bc/self.org
     ./programming-notes/terminal/tools/roff/pic/self.org
     ./programming-notes/languages/stack/postscript/self.org
     # ./programming-notes/languages/sql/sqlite/self.org
