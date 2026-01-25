88b16906079f88b960b3047a0d6cf91e4ec7a805
azimut <azimut.github@protonmail.com>
2026-01-25 17:31:15 -0300

    draft: sst29 final draft

--- org/blog/drafts/sst29-mega.ino
+++ org/blog/drafts/sst29-mega.ino
@@ -1,8 +1,9 @@
-#define WE 14
-#define OE 15
+#define NBLOCKS 5
+#define WE 4
+#define OE 3
 
 // Port Registers:
-// ←→ A = data
+// <- A = data
 // -> C = addr lower
 // -> L = addr higher
 
@@ -14,31 +15,31 @@ byte data[] = {
 };
 
 void setAddress(word addr) {
-  DDRC = DDRL = 0xff; // 1 = OUTPUT
-  PORTC = addr & 0x00ff;
-  PORTL = addr >> 8;
+  DDRC = 0xff; // 1 = OUTPUT
+  DDRL = 0xff; // 1 = OUTPUT
+  PORTC = (addr >> 0) & 0x00ff;
+  PORTL = (addr >> 8) & 0x00ff;
 }
 
-void writeEEPROM(int addr, byte data) {
+void writeEEPROM(int addr, byte wdata) {
   digitalWrite(OE, HIGH);
   setAddress(addr);
   digitalWrite(WE, LOW);
   DDRA = 0xff; // 1 = pinMode = OUTPUT
-  PORTA = data;
+  PORTA = wdata;
   delayMicroseconds(1);
   digitalWrite(WE, HIGH);
-  /* delay(10); */ // FIX: Sometimes needed?
 }
 
 byte readEEPROM(int addr) {
-  byte data = 0;
+  byte rdata = 0;
   DDRA = 0x00; // 0 = pinMode = INPUT
   setAddress(addr);
   digitalWrite(WE, HIGH);
   digitalWrite(OE, LOW);
-  data = PINA;
+  rdata = PINA;
   digitalWrite(OE, HIGH);
-  return data;
+  return rdata;
 }
 
 void setup() {
@@ -46,27 +47,50 @@ void setup() {
   pinMode(WE, OUTPUT); digitalWrite(WE, HIGH);
   pinMode(OE, OUTPUT); digitalWrite(OE, HIGH);
 
-  DDRC = DDRL = 0xff; // 1 = OUTPUT
+  DDRC = 0xff; // 1 = OUTPUT
+  DDRL = 0xff; // 1 = OUTPUT
 
   Serial.begin(57600);
   while(!Serial);
 
+  /* Serial.print("\nDisabling SDP..."); */
+  /* disable_sdp(); */
+  /* Serial.println(" done"); */
+
+  // Dump
   printContents();
-  Serial.print("Programming...");
-  int s = 127;
+  // Programming
+  Serial.print("Programming ... ");
   for (word address = 0; (address < sizeof(data)); address++) {
+    // Wait after each TBLCO (Byte Load Cycle Time)
+    if (address != 0 && address % 128 == 0) {
+      while (readEEPROM(address-1) != readEEPROM(address-1)) {
+        delayMicroseconds(100);
+      };
+    }
     writeEEPROM(address, data[address]);
   }
-  for (word address = sizeof(data); (address <= s); address++) {
-    writeEEPROM(address, 0xea);
+  // Padding
+  for (word address = sizeof(data); address % 16 != 0; address++) {
+    writeEEPROM(address, 0x00); // NOP = 0x00
   }
-  while (readEEPROM(s) != readEEPROM(s)) {
-    Serial.print(".");
-    delayMicroseconds(100);
-  };
-  Serial.println(" done");
-
-  delay(100);
+  Serial.println("done");
+  delay(500);
+  // Validation
+  Serial.print("Validating...");
+  byte okvalidation = 0;
+  for (word address = 0; address < sizeof(data); address++) {
+    if (readEEPROM(address) != data[address]) {
+      okvalidation = 1;
+      break;
+    }
+  }
+  if (okvalidation != 0) {
+    Serial.println("FAIL");
+  } else {
+    Serial.println("OK");
+  }
+  // Dump
   printContents();
 }
 
@@ -74,15 +98,37 @@ void loop() {
 }
 
 void printContents() {
-  for (int base = 0; base <= 255; base += 16) {
-    byte data[16];
+  for (int base = 0; base <= (NBLOCKS * 127)+1; base += 16) {
+    byte bdata[16];
     for (int offset = 0; offset <= 15; offset += 1) {
-      data[offset] = readEEPROM(base + offset);
+      bdata[offset] = readEEPROM(base + offset);
     }
-    char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
-            base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-    Serial.println(buf);
+    char sbuf[80];
+    sprintf(sbuf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+            base, bdata[0], bdata[1], bdata[2], bdata[3], bdata[4], bdata[5], bdata[6], bdata[7],
+            bdata[8], bdata[9], bdata[10], bdata[11], bdata[12], bdata[13], bdata[14], bdata[15]);
+    Serial.println(sbuf);
   }
+}
+
+void disable_sdp() {
+  digitalWrite(OE, HIGH);
+  DDRA = 0xff;
+  setByte(0x5555, 0xaa);
+  setByte(0x2aaa, 0x55);
+  setByte(0x5555, 0x80); // 0x80
+  setByte(0x5555, 0xaa);
+  setByte(0x2aaa, 0x55);
+  setByte(0x5555, 0x20); // 0x20
+  DDRA = 0x00;
+  delay(10);
+}
+
+void setByte(int addr, byte data) {
+  setAddress(addr); // digitalWrite(OE, HIGH);
+  PORTA = data;
+  delayMicroseconds(1);
+  digitalWrite(WE, LOW);
+  delayMicroseconds(1);
+  digitalWrite(WE, HIGH);
 }
